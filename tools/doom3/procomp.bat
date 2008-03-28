512b0517968b8947baa62b2c5cd81d780aa514c5
gdemont <gdemont@c4d228d6-d88a-4964-8f71-aa44024e0872>
2008-03-28 14:05:19 +0000

    renamed obj/gnatopti into obj/gnatfast

--- tools/doom3/procomp.bat
+++ tools/doom3/procomp.bat
@@ -1,4 +1,4 @@
 d3a %2 %3 %4 %1.proc >%1.ada 
 gnatchop -w %1.ada
-gnatmake %1 -gnatp -O2 -fomit-frame-pointer -I..\..\src -I..\..\bindings -I..\..\src\unzip -i -aO..\..\obj\gnatopti
+gnatmake %1 -gnatp -O2 -fomit-frame-pointer -I..\..\src -I..\..\bindings -I..\..\src\unzip -i -aO..\..\obj\gnatfast
 
