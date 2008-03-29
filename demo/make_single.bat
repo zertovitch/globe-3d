08a23fe2165c689fd8fa65ce1123013fdfe790cf
gdemont <gdemont@c4d228d6-d88a-4964-8f71-aa44024e0872>
2008-03-29 13:09:08 +0000

    win32 batch and big demo update

--- demo/make_single.bat
+++ demo/make_single.bat
@@ -3,54 +3,21 @@
 echo ----- GLOBE_3D: Make command for Win32.
 echo.
 
-if "%1" == "" echo Usage: make_1 (unit)
+if "%1" == "" echo Usage: make_single (unit)
 if "%1" == "" goto fin
 
 echo ----- Procedure to be built: %1
 echo.
 
-rem ** Common gnatmake / compiler options
-set compiler=-i -gnatecg3d_elim.pra
-set src=-aI../src -aI../src/gl -aI../src/unzip -aI../src/models -aI../src/gaming -aI../bindings -aI../bindings/win32 -aI../bindings/win32/gcc_fmt -aI../ada05_proxy
-set obj=-lopengl32 ../obj/libwin32/glee.o -lglu32 -lfreeglut -L../obj/libwin32
+set scenario_324943981475=Small
 
-rem -Wl,--stack=20000000
+echo -----------------------------------------------
+echo ---- Compile mode: %scenario_324943981475%
+echo -----------------------------------------------
 
-goto small
+gnatmake -PGLOBE_3D_GPS_Win32.gpr %1.adb -d -XBuild_Mode=%scenario_324943981475%
 
-:debug
-rem  -----------------------------------------------
-echo ---- compile mode: DEBUG
-rem  -----------------------------------------------
-
-rem gnatmake %compiler% -g -j2 -fstack-check -gnato -gnatwa -gnatVa -gnatecdebug.pra -fno-strict-aliasing %src% -aO../obj/gnatdebg %1 %2 %3 -bargs -E -largs %obj%
-gnatmake -PGLOBE_3D_GPS_Win32.gpr GLOBE_3D_Demo.adb -d -XBuild_Mode=Debug 
-goto done
-
-:fast
-rem  -----------------------------------------------
-echo ---- compile mode: OPTIM: FAST
-rem  -----------------------------------------------
-
-rem gnatmake %compiler% -O2 -j2 -gnatpn -funroll-loops -fpeel-loops -ftracer -funswitch-loops -fno-strict-aliasing %src% -aO../obj/gnatfast %1 %2 %3 -largs %obj% -s -mwindows
-gnatmake -PGLOBE_3D_GPS_Win32.gpr GLOBE_3D_Demo.adb -d -XBuild_Mode=Fast 
-
-:small
-rem  -----------------------------------------------
-echo ---- compile mode: OPTIM: SMALL
-rem  -----------------------------------------------
-
-rem ** Set an old version of GNAT (small .exe, Win 9x compatible)
-rem ** Trick silent if alternative GNAT not installed...
-rem path f:\ada\gnat315p\bin;%path%
-
-rem gnatmake %compiler% -O2 -s -fomit-frame-pointer -gnatp %src% -aO../obj/gnatsmal %1 %2 %3 -largs %obj% -s -mwindows
-gnatmake -PGLOBE_3D_GPS_Win32.gpr GLOBE_3D_Demo.adb -d -XBuild_Mode=Small 
-
-rem -----------------------------------------------
-:done
-
-rem GNAT 2005 -fno-strict-aliasing
+set scenario_324943981475=
 
 echo.
 
