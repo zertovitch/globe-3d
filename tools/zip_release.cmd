a2f777c8bc7bc33f4c1d674f261daa28e260b7b1
gdemont <gdemont@c4d228d6-d88a-4964-8f71-aa44024e0872>
2012-07-16 00:23:32 +0000

    Some script brush-up

--- tools/zip_release.cmd
+++ tools/zip_release.cmd
@@ -1,10 +1,17 @@
+del *.bak
+
 call upx_the_demo
 
 cd..
+copy /B demo\freeglut.dll demo\culler\terrain_vbo
+copy /B demo\freeglut.dll demo\sprite
+
+
 cd obj
 cd gnatdebg
 del *.ali
 del *.o
+del b~*.ad*
 cd ..
 cd gnatsmal
 del *.ali
@@ -17,4 +24,9 @@ cd ..
 cd ..
 cd ..
 
-zip -9 -r GLOBE_3D.zip GLOBE_3D\*
+rem ren globe3d GLOBE_3D
+zip -9 -r GLOBE_3D.zip GLOBE3D\*
+rem ren globe_3d globe3d
+deflopt GLOBE_3D.zip
+
+pause
