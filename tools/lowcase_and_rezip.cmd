d8febdaeb8971e67c0eba2d39782c2adb1b71074
gdemont <gdemont@c4d228d6-d88a-4964-8f71-aa44024e0872>
2012-07-17 01:01:10 +0000

    Packing sripts brush-up for release

--- tools/lowcase_and_rezip.cmd
+++ tools/lowcase_and_rezip.cmd
@@ -1,5 +1,8 @@
 rem This is for the upload of GLOBE_3D.
 
+cd..
+cd..
+
 rename globe_3d.zip globe_3d.zip
 
 echo The upload or version date of this archive's GLOBE_3D is: %date% >_GLOBE_3D_Upload_version_%date%.txt
@@ -8,12 +11,20 @@ zip -9 globe_3d _GLOBE_3D_Upload_version_%date%.txt
 
 del _GLOBE_3D_Upload_version_%date%.txt
 
-goto skip_rezip
+rem goto skip_rezip
 
-rezip globe_3d.zip
-deflopt globe_3d.zip
+rezip -defl -comp globe_3d.zip
+del globe_3d.old.zip
+ren globe_3d.zip globe_3d.old.zip
+ren globe_3d.repacked.zip globe_3d.zip
+pause
 
 :skip_rezip
 
 type c:\ada\unpack.msg | zip -z globe_3d.zip
+
+cd globe3d
+cd globe_3d
+cd tools
+
 pause
--- tools/zip_release.cmd
+++ tools/zip_release.cmd
@@ -6,7 +6,6 @@ cd..
 copy /B demo\freeglut.dll demo\culler\terrain_vbo
 copy /B demo\freeglut.dll demo\sprite
 
-
 cd obj
 cd gnatdebg
 del *.ali
@@ -25,7 +24,9 @@ cd ..
 cd ..
 
 rem ren globe3d GLOBE_3D
-zip -9 -r GLOBE_3D.zip GLOBE3D\*
+rem We prefer with '_', the package & project name
+rem Then, svn checkout with '_' and everything is fine ;-)
+zip -9 -r GLOBE_3D.zip GLOBE_3D\*
 rem ren globe_3d globe3d
 deflopt GLOBE_3D.zip
 
