c04903bb4cdf72db1f2925e29eb45657ebd40926
gdemont <gdemont@c4d228d6-d88a-4964-8f71-aa44024e0872>
2016-06-18 11:33:18 +0000

    GLOBE_3D.Collision_detection: added detection on sub-objects

--- src/globe_3d-collision_detection.adb
+++ src/globe_3d-collision_detection.adb
@@ -45,105 +45,116 @@ package body GLOBE_3D.Collision_detection is
     reacted     : out Real          -- in proportion to step
   )
   is
-    use GL.Math;
-    P_after_step, P_face: Point_3D;
-    u,n : Vector_3D;
-    dist_after, dist_before, nn: Real; -- distance orientee
-    retour: Real:= 0.0;
-    lstep0: constant Real:= Norm(step);
+    procedure Reaction_internal(o: Object_3D'Class) is
+      use GL.Math;
+      P_after_step, P_face: Point_3D;
+      u,n : Vector_3D;
+      dist_after, dist_before, nn: Real; -- distance orientee
+      retour: Real:= 0.0;
+      lstep0: constant Real:= Norm(step);
+      so: p_Object_3D_list:= o.sub_objects;
 
-    -- This function check whether we are inside the prism above face f
+      -- This function check whether we are inside the prism above face f
 
-    function Dans_prisme_epaissi(f: Positive) return Boolean is
-      sfp1: Positive;
-      Ps, Psp1: Point_3D;
-      u, edge_vector, npa: Vector_3D;
-      dist_edge, nnpa: Real;
-      facteur: constant:= 1.05;
+      function Dans_prisme_epaissi(f: Positive) return Boolean is
+        sfp1: Positive;
+        Ps, Psp1: Point_3D;
+        u, edge_vector, npa: Vector_3D;
+        dist_edge, nnpa: Real;
+        facteur: constant:= 1.05;
+      begin
+        -- Cycle through face's vertices
+        for sf in reverse 1..o.face_internal(f).last_edge loop
+          sfp1:= 1 + sf mod o.face_internal(f).last_edge;
+          Ps  := o.point( o.face_internal(f).P_compact(sf)   );
+          Psp1:= o.point( o.face_internal(f).P_compact(sfp1) );
+          edge_vector:= Psp1 - Ps;
+          npa:= n * edge_vector;
+          nnpa:= Norm(npa);
+          if Almost_zero(nnpa) then -- degenerated edge
+            return False;
+          end if;
+          npa:= 1.0/nnpa * npa;
+          -- npa points towards the prism's interior
+          u:= P_after_step - (Ps + o.centre);
+          dist_edge:= u * npa;
+          if dist_edge < - ball.radius * facteur then
+            return False;
+          end if;
+        end loop;
+        return True;
+      end Dans_prisme_epaissi;
+
     begin
-      -- Cycle through face's vertices
-      for sf in reverse 1..o.face_internal(f).last_edge loop
-        sfp1:= 1 + sf mod o.face_internal(f).last_edge;
-        Ps  := o.point( o.face_internal(f).P_compact(sf)   );
-        Psp1:= o.point( o.face_internal(f).P_compact(sfp1) );
-        edge_vector:= Psp1 - Ps;
-        npa:= n * edge_vector;
-        nnpa:= Norm(npa);
-        if Almost_zero(nnpa) then -- degenerated edge
-          return False;
-        end if;
-        npa:= 1.0/nnpa * npa;
-        -- npa points towards the prism's interior
-        u:= P_after_step - (Ps + o.centre);
-        dist_edge:= u * npa;
-        if dist_edge < - ball.radius * facteur then
-          return False;
-        end if;
-      end loop;
-      return True;
-    end Dans_prisme_epaissi;
-
-  begin
-    reacted:= 0.0;
-    if Almost_zero(lstep0) then
-      return;
-    end if;
-
-    P_after_step:= ball.centre + step;
-
-    for face in reverse 1..o.Max_faces loop
-      n:= o.face_internal(face).normal;
-      if check_normals then
-        nn:= Norm(n);
-        if Almost_zero(nn) then
-          raise Zero_normal;
-        elsif abs(nn - 1.0) > 1.0e-7 then
-          raise Not_one_normal with " norm = " & Real'Image(nn);
-        end if;
+      if Almost_zero(lstep0) then
+        return;
       end if;
-      --  put_line("step=" & step(0)'img & ' ' & step(1)'img & ' ' & step(2)'img);
-      --  put_line("   n=" & n(0)'img & ' ' & n(1)'img & ' ' & n(2)'img);
-      if step * n < 0.0 then
-        P_face:= o.point(o.face_internal(face).P_compact(1)) + o.centre;
-        -- ^ any point on the face, to measure distance to face's plane.
-        u:= ball.centre - P_face;
-        dist_before:= u * n;
-        if dist_before > 0.0 then
-          -- ^ Fine, we are on the right side of the face.
-          --   Test added to Engine_3D's algo, since objects are
-          --   not always hollow, convex polyhedrons anymore.
-          u:= P_after_step - P_face;
-          dist_after:= u * n;
-          if dist_after < ball.radius
-            -- ^ Ouch! React we must!
-            -- This includes negatives values of dist_after, in cases
-            -- the intended step makes going through the face!
-          and then
-             Dans_prisme_epaissi(face)
-          then
-            if o.face(face).skin /= invisible then
-            -- ^ this assumes: invisible <=> can go through
-              reacted:= reacted + retour / lstep0;
-              -- !! seems wrong if reactions in different directions
-              --    should be something like step * step0
-              case method is
-                when elastic =>
-                  raise Unsupported with "elastic reaction";
-                  -- should compute the time the "ball" takes from rebound to
-                  -- next face or portal.
-                when slide =>
-                  retour:= ball.radius - dist_after; -- always > 0
-                  step:= step + retour * n;
-                  -- Since step and n have a negative dot product      -checked-
-                  -- and dist(ball.centre+step_old,face) < ball.radius -checked-
-                  -- then:
-                  -- ||step_new|| < ||step_old|| --> decreasing algo :-)
-              end case;
+
+      P_after_step:= ball.centre + step;
+
+      for face in reverse 1..o.Max_faces loop
+        n:= o.face_internal(face).normal;
+        if check_normals then
+          nn:= Norm(n);
+          if Almost_zero(nn) then
+            raise Zero_normal;
+          elsif abs(nn - 1.0) > 1.0e-7 then
+            raise Not_one_normal with " norm = " & Real'Image(nn);
+          end if;
+        end if;
+        --  put_line("step=" & step(0)'img & ' ' & step(1)'img & ' ' & step(2)'img);
+        --  put_line("   n=" & n(0)'img & ' ' & n(1)'img & ' ' & n(2)'img);
+        if step * n < 0.0 then
+          P_face:= o.point(o.face_internal(face).P_compact(1)) + o.centre;
+          -- ^ any point on the face, to measure distance to face's plane.
+          u:= ball.centre - P_face;
+          dist_before:= u * n;
+          if dist_before > 0.0 then
+            -- ^ Fine, we are on the right side of the face.
+            --   Test added to Engine_3D's algo, since objects are
+            --   not always hollow, convex polyhedrons anymore.
+            u:= P_after_step - P_face;
+            dist_after:= u * n;
+            if dist_after < ball.radius
+              -- Ouch! Collision! React we must!
+              --
+              -- This includes negatives values of dist_after, in cases
+              -- the intended step makes going through the face!
+            and then
+               Dans_prisme_epaissi(face)
+            then
+              if o.face(face).skin /= invisible then
+              -- ^ this assumes: invisible <=> can go through
+                reacted:= reacted + retour / lstep0;
+                -- !! seems wrong if reactions in different directions
+                --    should be something like step * step0
+                case method is
+                  when elastic =>
+                    raise Unsupported with "elastic reaction";
+                    -- should compute the time the "ball" takes from rebound to
+                    -- next face or portal.
+                  when slide =>
+                    retour:= ball.radius - dist_after; -- always > 0
+                    step:= step + retour * n;
+                    -- Since step and n have a negative dot product      -checked-
+                    -- and dist(ball.centre+step_old,face) < ball.radius -checked-
+                    -- then:
+                    -- ||step_new|| < ||step_old|| --> decreasing algo :-)
+                end case;
+              end if;
             end if;
           end if;
         end if;
-      end if;
-    end loop;
+      end loop;
+      while so /= null loop
+        Reaction_internal(so.objc.all);
+        so:= so.next;
+      end loop;
+    end Reaction_internal;
+
+ begin
+   reacted:= 0.0;
+   Reaction_internal(o);
  end Reaction;
 
 end GLOBE_3D.Collision_detection;
