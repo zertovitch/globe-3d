84d84c28cec6f850011f1f5cf71a96d163f120d3
rodkay <rodkay@c4d228d6-d88a-4964-8f71-aa44024e0872>
2016-06-25 11:42:52 +0000

    Tidy of 'globe_3d.sprite' spec & body

--- src/objects/globe_3d-sprite.adb
+++ src/objects/globe_3d-sprite.adb
@@ -1,10 +1,4 @@
---  with GLOBE_3D.Textures,
---       GLOBE_3D.Math;
-
 package body GLOBE_3D.Sprite is
-
-   --  package G3DT renames GLOBE_3D.Textures;
-   --  package G3DM renames GLOBE_3D.Math;
 
    function skinned_Geometries (o : in Sprite) return GL.Skinned_Geometry.Skinned_Geometries
    is
@@ -15,7 +9,6 @@ package body GLOBE_3D.Sprite is
    procedure add (o : in out Sprite;   Geometry : access GL.Geometry.Geometry'Class;
                                        Skin     : access GL.Skins.Skin'Class)
    is
-      --  new_skinned_Geometry : access GL.Skinned_Geometry.Skinned_Geometry := new GL.Skinned_Geometry.Skinned_Geometry;
    begin
       o.skinned_geometry_Count                        := o.skinned_geometry_Count + 1;
       o.skinned_Geometries (o.skinned_geometry_Count) := (Geometry => Geometry.all'Access,
@@ -30,27 +23,14 @@ package body GLOBE_3D.Sprite is
       --vertex_cache_optimise (o);  -- tbd: doesn't seem to help !! ... :(
                                   -- at least with terrain ... (terrain dataset may already naturally be in optimal order ?)
                                   -- so need to test with other dataset
-
       o.Bounds     := null_Bounds;
       o.face_Count := 0;
 
-      for Each in 1 .. o.skinned_geometry_Count loop
+      for Each in 1 .. o.skinned_geometry_Count
+      loop
          o.Bounds     := Max (o.Bounds,  o.skinned_Geometries (Each).Geometry.Bounds);
          o.face_Count := o.face_Count + o.skinned_Geometries (Each).Geometry.face_Count;
       end loop;
-
-      -- setup bounding_sphere (for debug)
-      --
---        declare
---           use GLU;
---        begin
---           if o.bounding_sphere_Quadric /= null then
---              glu.quadricDrawStyle (o.bounding_sphere_Quadric, glu.glu_LINE);
---           end if;
---        end;
-
-    -- Ooof. Now we can certify:
-      --o.pre_calculated:= True;
    end Pre_calculate;
 
    procedure destroy (o : in out Sprite)
@@ -75,20 +55,20 @@ package body GLOBE_3D.Sprite is
                       clip : in     Clipping_data)
    is
    begin
-      null;   -- actual display is done by the renderer (ie glut.Windows), which requests all skinned Geometry's
+      null;   -- Actual display is done by the renderer (ie glut.Windows), which requests all skinned Geometry's
               -- and then applies 'gl state' sorting for performance, before drawing.
    end Display;
 
    procedure set_Alpha (o    : in out Sprite;   Alpha : in GL.Double)
    is
    begin
-      null;   -- tbd
+      null;   -- todo
    end;
 
    function  is_Transparent (o    : in Sprite) return Boolean
    is
    begin
-      return o.is_Transparent;  -- tbd: ensure this is updated when new primitives (with possible transparent appearance' are added.
+      return o.is_Transparent;  -- todo: ensure this is updated when new primitives (with possible transparent appearance' are added.
    end;
 
 end GLOBE_3D.Sprite;
--- src/objects/globe_3d-sprite.ads
+++ src/objects/globe_3d-sprite.ads
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------------
 --  GLOBE_3D.Sprite
 --
---  Copyright (c) Rod Kay 2007
+--  Copyright (c) Rod Kay 2007+
 --  AUSTRALIA
 --
 --  Permission granted to use this software, without any warranty,
@@ -9,15 +9,18 @@
 --  and unmodified if sources are distributed further.
 -------------------------------------------------------------------------
 
-with GL,  GL.Geometry,  GL.Skins,  GL.Skinned_Geometry;
+with
+     GL.Geometry,
+     GL.Skins,
+     GL.Skinned_Geometry;
 
 package GLOBE_3D.Sprite is
 
-   -- a 'Visual' which consists of multiple gl.skinned_Geometries
+   -- A 'Visual' which is composed of several 'gl.skinned_Geometry' items.
 
    type Sprite (max_Geometries : Positive) is new Visual with
       record
-         skinned_Geometries      : GL.Skinned_Geometry.Skinned_Geometries (1 .. max_Geometries);
+         skinned_Geometries     : GL.Skinned_Geometry.Skinned_Geometries (1 .. max_Geometries);
          skinned_geometry_Count : Natural := 0;
 
          -- private:
@@ -29,24 +32,21 @@ package GLOBE_3D.Sprite is
    type p_Sprite      is access all Sprite'Class;
    type p_sprite_Grid is array (Positive range <>, Positive range <>) of p_Sprite;
 
-   procedure destroy (o : in out Sprite);
+   procedure destroy        (o : in out Sprite);
 
-   procedure set_Alpha (o    : in out Sprite;   Alpha : in GL.Double);
-   function  is_Transparent (o    : in Sprite) return Boolean;
+   procedure set_Alpha      (o : in out Sprite;   Alpha : in GL.Double);
+   function  is_Transparent (o : in     Sprite) return Boolean;
 
    function skinned_Geometries (o : in Sprite) return GL.Skinned_Geometry.Skinned_Geometries;
 
-   procedure add (o : in out Sprite;   Geometry : access GL.Geometry.Geometry'Class;
-                                       Skin     : access GL.Skins.Skin'Class);
+   procedure add            (o : in out Sprite;   Geometry : access GL.Geometry.Geometry'Class;
+                                                  Skin     : access GL.Skins.Skin'Class);
 
-   procedure Pre_calculate (o: in out Sprite);
+   procedure Pre_calculate  (o : in out Sprite);
 
-   procedure Display(
-    o      : in out Sprite;
-    clip   : in     Clipping_data
-  );
+   procedure Display        (o : in out Sprite;   clip : in Clipping_data);
 
-   function face_Count (o : in Sprite) return Natural;
-   function Bounds     (o : in Sprite) return GL.Geometry.Bounds_record;
+   function face_Count      (o : in Sprite) return Natural;
+   function Bounds          (o : in Sprite) return GL.Geometry.Bounds_record;
 
 end GLOBE_3D.Sprite;
