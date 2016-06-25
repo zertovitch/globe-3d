de61ff5b64ab8d717683fba74b2d189296987fbc
rodkay <rodkay@c4d228d6-d88a-4964-8f71-aa44024e0872>
2016-06-25 08:59:49 +0000

    Tidy of 'globe_3d.impostor' body.

--- src/objects/globe_3d-impostor.adb
+++ src/objects/globe_3d-impostor.adb
@@ -1,19 +1,13 @@
---  with GLOBE_3D.Textures,
-with GLOBE_3D.Math;
-with GLOBE_3D.Visuals_rendering;
-
-with GL.Math;
-
---  with GLUT.Windows; use GLUT.Windows;
-with GL.Errors;
---  with GLU;
-
-with Ada.Text_IO;  use Ada.Text_IO;
+with
+     GLOBE_3D.Math,
+     GLOBE_3D.Visuals_rendering,
+     GL.Math,
+     GL.Errors,
+     Ada.Text_IO;
+use
+     Ada.Text_IO;
 
 package body GLOBE_3D.Impostor is
-
-   --  package G3DT renames GLOBE_3D.Textures;
-   --  package G3DM renames GLOBE_3D.Math;
 
    procedure destroy (o : in out Impostor)
    is
@@ -50,7 +44,7 @@ package body GLOBE_3D.Impostor is
 
       Target.Pre_calculate;
 
-      -- set o.skinned_Geometry.geometry.vertices & indices
+      -- Set o.skinned_Geometry.geometry.vertices & indices.
       --
       declare
         Width : constant GL.Double := Target.Bounds.sphere_Radius * 1.00;
@@ -61,23 +55,19 @@ package body GLOBE_3D.Impostor is
          o.Quads.Vertices (4) := (-Width,  Width, 0.0);
       end;
 
-      o.Quads.all.set_vertex_Id (1, 1,  1);      -- tbd: the '.all' required for gnat gpl06 ... not required in gpl07.
+      o.Quads.all.set_vertex_Id (1, 1,  1);      -- todo: the '.all' required for gnat gpl06 ... not required in gpl07.
       o.Quads.all.set_vertex_Id (1, 2,  2);
       o.Quads.all.set_vertex_Id (1, 3,  3);
       o.Quads.all.set_vertex_Id (1, 4,  4);
 
-      -- create the veneer, if necessary
+      -- Create the veneer, if necessary.
       --
       if o.skinned_Geometry.Veneer = null then
-         --o.skinned_Geometry.Veneer := o.skinned_Geometry.Skin.new_Veneer (o.Quads.all);
          o.skinned_Geometry.Veneer := o.skinned_Geometry.Skin.new_Veneer (o.skinned_Geometry.Geometry.all);
       end if;
-
-      --o.bounding_sphere_Radius := bounding_sphere_Radius (o.Quads.vertex_Pool.all);
-      --o.Bounds := o.skinned_Geometry.Geometry.Bounds;
    end;
 
-   -- update trigger configuration
+   -- Update Trigger Configuration
    --
 
    procedure set_freshen_count_update_trigger_Mod (o : in out Impostor;   To : in Positive)
@@ -107,7 +97,8 @@ package body GLOBE_3D.Impostor is
    function general_Update_required (o : access Impostor;   the_Camera       : in p_Camera;
                                                             the_pixel_Region : in pixel_Region) return Boolean
    is
-   pragma Unreferenced (the_pixel_Region);
+      pragma Unreferenced (the_pixel_Region);
+
       use GL, GLOBE_3D.Math, GL.Math;
       Camera_has_moved : constant Boolean  :=  the_Camera.clipper.eye_position /= o.prior_camera_Position;
       Target_has_moved : constant Boolean  :=  o.Target.centre                 /= o.prior_target_Position;
@@ -130,9 +121,9 @@ package body GLOBE_3D.Impostor is
          return True;
       end if;
 
-      if         o.prior_pixel_Region.Width  >  40                -- ignore target rotation triggered updates when target is small on screen
+      if         o.prior_pixel_Region.Width  >  40                -- Ignore target rotation triggered updates when target is small on screen.
         and then o.prior_pixel_Region.Height >  40                --
-        and then o.prior_target_Rotation     /= o.Target.rotation
+        and then o.prior_target_Rotation    /= o.Target.rotation
       then
          return True;
       end if;
@@ -158,37 +149,34 @@ package body GLOBE_3D.Impostor is
       target_Centre_proj       : constant Vector_4D    := the_Camera.projection_matrix * target_Centre;
       target_Lower_Left_proj   : constant Vector_4D    := the_Camera.projection_matrix * target_lower_Left;
 
-      target_Centre_norm       : constant Vector_3D    :=
-                                                 (target_Centre_proj (0) / target_Centre_proj (3),
-                                                  target_Centre_proj (1) / target_Centre_proj (3),
-                                                  target_Centre_proj (2) / target_Centre_proj (3));
-                                                  target_Lower_Left_norm   : constant Vector_3D    := (target_Lower_Left_proj (0) / target_Lower_Left_proj (3),
-                                                  target_Lower_Left_proj (1) / target_Lower_Left_proj (3),
-                                                  target_Lower_Left_proj (2) / target_Lower_Left_proj (3));
+      target_Centre_norm       : constant Vector_3D    := (target_Centre_proj (0)     / target_Centre_proj (3),
+                                                           target_Centre_proj (1)     / target_Centre_proj (3),
+                                                           target_Centre_proj (2)     / target_Centre_proj (3));
+      target_Lower_Left_norm   : constant Vector_3D    := (target_Lower_Left_proj (0) / target_Lower_Left_proj (3),
+                                                           target_Lower_Left_proj (1) / target_Lower_Left_proj (3),
+                                                           target_Lower_Left_proj (2) / target_Lower_Left_proj (3));
 
-      target_Centre_norm_0to1  : constant Vector_3D    :=
-                                                 (target_Centre_norm (0)     * 0.5 + 0.5,
-                                                  target_Centre_norm (1)     * 0.5 + 0.5,
-                                                  target_Centre_norm (2)     * 0.5 + 0.5);
-      target_Lower_Left_norm_0to1 : constant Vector_3D :=
-                                                 (target_Lower_Left_norm (0) * 0.5 + 0.5,
-                                                  target_Lower_Left_norm (1) * 0.5 + 0.5,
-                                                  target_Lower_Left_norm (2) * 0.5 + 0.5);
+      target_Centre_norm_0to1  : constant Vector_3D    := (target_Centre_norm (0)     * 0.5 + 0.5,
+                                                           target_Centre_norm (1)     * 0.5 + 0.5,
+                                                           target_Centre_norm (2)     * 0.5 + 0.5);
+      target_Lower_Left_norm_0to1 : constant Vector_3D := (target_Lower_Left_norm (0) * 0.5 + 0.5,
+                                                           target_Lower_Left_norm (1) * 0.5 + 0.5,
+                                                           target_Lower_Left_norm (2) * 0.5 + 0.5);
 
       viewport_Width           : constant Integer      := the_Camera.clipper.main_clipping.X2 - the_Camera.clipper.main_clipping.X1 + 1;
       viewport_Height          : constant Integer      := the_Camera.clipper.main_clipping.Y2 - the_Camera.clipper.main_clipping.Y1 + 1;
 
       Width                    : constant Real         := 2.0  *  Real (viewport_Width) * (target_Centre_norm_0to1 (0) - target_Lower_Left_norm_0to1 (0));
       Width_pixels             : constant GL.Sizei     := GL.Sizei (  Integer (Real (viewport_Width) * target_Lower_Left_norm_0to1 (0) + Width)
-                                                           - Integer (Real (viewport_Width) * target_Lower_Left_norm_0to1 (0))
-                                                           + 1);
+                                                                    - Integer (Real (viewport_Width) * target_Lower_Left_norm_0to1 (0))
+                                                                    + 1);
 
       Height                   : constant Real         := 2.0  *  Real (viewport_Height) * (target_Centre_norm_0to1 (1) - target_Lower_Left_norm_0to1 (1));
       Height_pixels            : constant GL.Sizei     := GL.Sizei (  Integer (Real (viewport_Height) * target_Lower_Left_norm_0to1 (1) + Height)
-                                                           - Integer (Real (viewport_Height) * target_Lower_Left_norm_0to1 (1))
-                                                           + 1);
+                                                                    - Integer (Real (viewport_Height) * target_Lower_Left_norm_0to1 (1))
+                                                                    + 1);
    begin
-      o.all.target_camera_Distance := Norm (target_Centre);   -- nb: cache distance from camera to target.
+      o.all.target_camera_Distance := Norm (target_Centre);   -- nb: Cache distance from camera to target.
 
       return (X      => GL.Int (target_Lower_Left_norm_0to1 (0) * Real (viewport_Width)),
               Y      => GL.Int (target_Lower_Left_norm_0to1 (1) * Real (viewport_Height)),
@@ -202,25 +190,27 @@ package body GLOBE_3D.Impostor is
    is
       use GL, GL.Textures;
 
-Width_size               : constant GL.Textures.Size := to_Size (Natural (o.current_Width_pixels));
-Height_size              : constant GL.Textures.Size := to_Size (Natural (o.current_Height_pixels));
+      Width_size     : constant GL.Textures.Size := to_Size (Natural (o.current_Width_pixels));
+      Height_size    : constant GL.Textures.Size := to_Size (Natural (o.current_Height_pixels));
 
-texture_Width            : constant GL.Sizei         := power_of_2_Ceiling (Natural (o.current_Width_pixels));
-texture_Height           : constant GL.Sizei         := power_of_2_Ceiling (Natural (o.current_Height_pixels));
+      texture_Width  : constant GL.Sizei         := power_of_2_Ceiling (Natural (o.current_Width_pixels));
+      texture_Height : constant GL.Sizei         := power_of_2_Ceiling (Natural (o.current_Height_pixels));
 
       GL_Error : Boolean;
+
    begin
-      o.prior_pixel_Region    := (o.current_copy_X, o.current_copy_Y,  o.current_Width_pixels, o.current_Height_pixels);
+      o.prior_pixel_Region    := (o.current_copy_X,       o.current_copy_Y,
+                                  o.current_Width_pixels, o.current_Height_pixels);
       o.prior_Width_pixels    := o.current_Width_pixels;
       o.prior_Height_pixels   := o.current_Height_pixels;
       o.prior_target_Rotation := o.Target.rotation;
       o.prior_target_Position := o.Target.centre;
       o.prior_camera_Position := the_Camera.clipper.eye_position;
 
-      GL.ClearColor (0.0,  0.0,  0.0,   0.0);
-      GLOBE_3D.Visuals_rendering.Render ((1 => o.Target),  the_Camera.all); -- render the target for subsequent copy to impostor texture.
+      GL.ClearColor (0.0, 0.0, 0.0,  0.0);
+      GLOBE_3D.Visuals_rendering.Render ((1 => o.Target),  the_Camera.all); -- Render the target for subsequent copy to impostor texture.
 
-      declare -- set texture coordinates for the veneer.
+      declare -- Set texture coordinates for the veneer.
          use GL.Skins;
          the_Veneer : constant p_Veneer_transparent_unlit_textured := p_Veneer_transparent_unlit_textured (o.skinned_Geometry.Veneer);
 
@@ -229,17 +219,18 @@ texture_Height           : constant GL.Sizei         := power_of_2_Ceiling (Natu
          X_last     : constant Real := Real (o.current_Width_pixels)  / Real (texture_Width)  - X_first;
          Y_last     : constant Real := Real (o.current_Height_pixels) / Real (texture_Height) - Y_first;
       begin
-         the_Veneer.texture_Coordinates := (1 => (S => X_first,     T => Y_first),
-                                            2 => (S => X_last,      T => Y_first),
-                                            3 => (S => X_last,      T => Y_last),
-                                            4 => (S => X_first,     T => Y_last));
+         the_Veneer.texture_Coordinates := (1 => (S => X_first, T => Y_first),
+                                            2 => (S => X_last,  T => Y_first),
+                                            3 => (S => X_last,  T => Y_last),
+                                            4 => (S => X_first, T => Y_last));
       end;
 
       if        Width_size  /= GL.Textures.Size_width  (o.Skin.Texture)
         or else Height_size /= GL.Textures.Size_height (o.Skin.Texture)
       then
          free (texture_Pool.all,  o.Skin.Texture);
-         o.Skin.all.Texture := new_Texture (texture_Pool,  Natural (texture_Width),  Natural (texture_Height));
+         o.Skin.all.Texture := new_Texture (texture_Pool,  Natural (texture_Width),
+                                                           Natural (texture_Height));
       end if;
 
       enable (o.Skin.all.Texture);
@@ -252,20 +243,20 @@ texture_Height           : constant GL.Sizei         := power_of_2_Ceiling (Natu
       GL.Errors.Log (error_Occurred => GL_Error);
 
       if GL_Error then
-         Put_Line ("x_Offset: " & GL.Int'Image (o.current_copy_X_Offset) & " ********");
-         Put_Line ("y_Offset: " & GL.Int'Image (o.current_copy_Y_Offset));
+         Put_Line ("x_Offset: "       & GL.Int'Image (o.current_copy_X_Offset) & " ********");
+         Put_Line ("y_Offset: "       & GL.Int'Image (o.current_copy_Y_Offset));
 
-         Put_Line ("start x: " & GL.Int'Image (o.current_copy_X));
-         Put_Line ("start y: " & GL.Int'Image (o.current_copy_Y));
+         Put_Line ("start x: "        & GL.Int'Image (o.current_copy_X));
+         Put_Line ("start y: "        & GL.Int'Image (o.current_copy_Y));
 
-         Put_Line ("copy width: "  & GL.Sizei'Image (o.current_copy_Width));
-         Put_Line ("copy height: " & GL.Sizei'Image (o.current_copy_Height));
+         Put_Line ("copy width: "     & GL.Sizei'Image (o.current_copy_Width));
+         Put_Line ("copy height: "    & GL.Sizei'Image (o.current_copy_Height));
 
-         Put_Line ("width_pixels: "  & GL.Sizei'Image (o.current_Width_pixels));
-         Put_Line ("height_pixels: " & GL.Sizei'Image (o.current_Height_pixels));
+         Put_Line ("width_pixels: "   & GL.Sizei'Image (o.current_Width_pixels));
+         Put_Line ("height_pixels: "  & GL.Sizei'Image (o.current_Height_pixels));
 
-         Put_Line ("width_size: "  & GL.Textures.Size'Image (Width_size));
-         Put_Line ("height_size: " & GL.Textures.Size'Image (Height_size));
+         Put_Line ("width_size: "     & GL.Textures.Size'Image (Width_size));
+         Put_Line ("height_size: "    & GL.Textures.Size'Image (Height_size));
 
          Put_Line ("texture width: "  & GL.Sizei'Image (texture_Width));
          Put_Line ("texutre height: " & GL.Sizei'Image (texture_Height));
@@ -279,7 +270,7 @@ texture_Height           : constant GL.Sizei         := power_of_2_Ceiling (Natu
                                                    texture_Pool : in     GL.Textures.p_Pool;
                                                    is_Valid     :    out Boolean            )
    is
-      update_Required : constant Boolean := o.update_Required (the_Camera);    -- nb: caches current update info
+      update_Required : constant Boolean := o.update_Required (the_Camera);    -- nb: Caches current update info.
    begin
       if update_Required then
          o.update (the_Camera, texture_Pool);
@@ -328,14 +319,14 @@ texture_Height           : constant GL.Sizei         := power_of_2_Ceiling (Natu
    procedure Display (o : in out Impostor;   clip : in     Clipping_data)
    is
    begin
-      null;   -- actual display is done by the renderer (ie glut.Windows), which requests all skinned Geometry's
+      null;   -- Actual display is done by the renderer (ie glut.Windows), which requests all skinned Geometry's
               -- and then applies 'gl state' sorting for performance, before drawing.
    end Display;
 
    procedure set_Alpha (o    : in out Impostor;   Alpha : in GL.Double)
    is
    begin
-      null;   -- tbd
+      null;   -- todo
    end;
 
    function Bounds (o : in     Impostor) return GL.Geometry.Bounds_record
@@ -348,10 +339,10 @@ texture_Height           : constant GL.Sizei         := power_of_2_Ceiling (Natu
    is
    pragma Unreferenced (o);
    begin
-      return True;   -- tbd: - if using gl alpha test, depth sorting is not needed apparently.
-                     --        in which case this could be set to False, and treated as a non-transparent in g3d.render.
-                     --        may then be faster (?).
-                     --      - seems to make little difference ... test with different vid card.
+      return True;   -- todo: - if using gl alpha test, depth sorting is not needed apparently.
+                     --         in which case this could be set to False, and treated as a non-transparent in g3d.render.
+                     --         may then be faster (?).
+                     --       - seems to make little difference ... test with different vid card.
    end;
 
    function Skin (o : access Impostor) return GL.Skins.p_Skin_transparent_unlit_textured
@@ -367,11 +358,11 @@ texture_Height           : constant GL.Sizei         := power_of_2_Ceiling (Natu
       return p_Quads (p_primal_Geometry (o.skinned_Geometry.Geometry).Primitive);
    end;
 
-   -- note: only old, unused code folows (may be useful) ...
+   -- note: Only old, unused code folows (may be useful) ...
    --
 
-   -- tbd: enable_rotation is no good for impostors, since they must be aligned with the viewport
-   --      it might be useful for general billboards however !
+   -- todo: Enable_rotation is no good for impostors, since they must be aligned with the viewport.
+   --       It might be useful for general billboards however !
    --
    procedure enable_Rotation (o : in Impostor;   camera_Site : in Vector_3D)
    is
@@ -408,6 +399,6 @@ texture_Height           : constant GL.Sizei         := power_of_2_Ceiling (Natu
    end;
    pragma Unreferenced (enable_Rotation);
    --
-   -- based on lighthouse3d billboard example.
+   -- Based on lighthouse3d billboard example.
 
 end GLOBE_3D.Impostor;
