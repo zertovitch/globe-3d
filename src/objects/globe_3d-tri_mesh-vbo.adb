3c53e2c65a40b35833684220a5fd6bf848ae5107
rodkay <rodkay@c4d228d6-d88a-4964-8f71-aa44024e0872>
2016-06-25 12:35:57 +0000

    Tidy of 'globe_3d.tri_mesh.vbo' spec & body

--- src/objects/globe_3d-tri_mesh-vbo.adb
+++ src/objects/globe_3d-tri_mesh-vbo.adb
@@ -1,26 +1,12 @@
---  with GLOBE_3D.Options,
---       GLOBE_3D.Textures,
---       GLOBE_3D.Math;
-
-with GL.Buffer.Vertex;
-with GL.Buffer.Indices;
-with GL.Extended; use GL.Extended;
-
---  with GL.Buffer.Texture_coords;
-
---  with Ada.Exceptions; use Ada.Exceptions;
---  with Ada.Text_IO;    use Ada.Text_IO;
-
-with Ada.Unchecked_Conversion;
-
---  with System;
+with
+     GL.Buffer.Vertex,
+     GL.Buffer.Indices,
+     GL.Extended,
+     Ada.Unchecked_Conversion;
+use
+     GL.Extended;
 
 package body GLOBE_3D.tri_Mesh.VBO is
-
-  --  use GLOBE_3D.Options;
-
-  --  package G3DT renames GLOBE_3D.Textures;
-  --  package G3DM renames GLOBE_3D.Math;
 
    procedure destroy (o: in out tri_Mesh)
    is
@@ -35,24 +21,22 @@ package body GLOBE_3D.tri_Mesh.VBO is
       return (1 => o.skinned_Geometry);
    end;
 
-   procedure set_Alpha ( o    : in out tri_Mesh;   Alpha : in GL.Double)
+   procedure set_Alpha (o : in out tri_Mesh;   Alpha : in GL.Double)
    is
    begin
-      null;    -- tbd:
+      null;    -- todo
    end;
 
-   function  is_Transparent (o    : in tri_Mesh) return Boolean
+   function  is_Transparent (o : in tri_Mesh) return Boolean
    is
-      --  use type GL.Double;
    begin
       return o.skinned_Geometry.Skin.is_Transparent;
    end;
 
    procedure Pre_calculate (o: in out tri_Mesh)
    is
-      --  use GL, G3DM;
    begin
-      null;  -- tbd:
+      null;  -- todo
    end Pre_calculate;
 
    procedure Display (o      : in out tri_Mesh;
@@ -64,11 +48,14 @@ package body GLOBE_3D.tri_Mesh.VBO is
 
    procedure set_Vertices (Self : in out tri_Mesh;   To : access GL.Geometry.Vertex_array)
    is
-      use GL.Buffer.Vertex, GL.Geometry, GL.Geometry.VBO;
+      use GL.Buffer.Vertex,
+          GL.Geometry,
+          GL.Geometry.VBO;
 
-      the_Geometry : GL.Geometry.VBO.vbo_Geometry renames GL.Geometry.VBO.vbo_Geometry (Self.skinned_Geometry.Geometry.all);
+      the_Geometry : GL.Geometry.VBO.vbo_Geometry
+             renames GL.Geometry.VBO.vbo_Geometry (Self.skinned_Geometry.Geometry.all);
    begin
-      the_Geometry.Vertices     := to_Buffer (To, Usage => STATIC_DRAW);  -- tbd: check usage
+      the_Geometry.Vertices     := to_Buffer (To, Usage => STATIC_DRAW);  -- todo: check usage
       the_Geometry.vertex_Count := GL.Sizei  (To'Length);
 
       the_Geometry.Bounds := Bounds (To.all);
@@ -76,8 +63,11 @@ package body GLOBE_3D.tri_Mesh.VBO is
 
    procedure set_Indices  (Self : in out tri_Mesh;   To : access GL.Geometry.vertex_Id_array)
    is
-      use GL.Buffer.Indices, GL.Geometry, GL.Geometry.VBO;
-      the_Geometry : GL.Geometry.VBO.vbo_Geometry renames GL.Geometry.VBO.vbo_Geometry (Self.skinned_Geometry.Geometry.all);
+      use GL.Buffer.Indices,
+          GL.Geometry,
+          GL.Geometry.VBO;
+      the_Geometry : GL.Geometry.VBO.vbo_Geometry
+             renames GL.Geometry.VBO.vbo_Geometry (Self.skinned_Geometry.Geometry.all);
    begin
       the_Geometry.indices_Count := GL.Sizei (To'Length);
       the_Geometry.Indices       := to_Buffer (To, Usage => STATIC_DRAW);
@@ -93,7 +83,8 @@ package body GLOBE_3D.tri_Mesh.VBO is
    function face_Count (o : in tri_Mesh) return Natural
    is
       use GL;
-      the_Geometry : GL.Geometry.VBO.vbo_Geometry renames GL.Geometry.VBO.vbo_Geometry (o.skinned_Geometry.Geometry.all);
+      the_Geometry : GL.Geometry.VBO.vbo_Geometry
+             renames GL.Geometry.VBO.vbo_Geometry (o.skinned_Geometry.Geometry.all);
    begin
       return Natural (the_Geometry.indices_Count / 3);
    end;
--- src/objects/globe_3d-tri_mesh-vbo.ads
+++ src/objects/globe_3d-tri_mesh-vbo.ads
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------------
 --  GLOBE_3D.tri_Mesh.VBO
 --
---  Copyright (c) Rod Kay 2007
+--  Copyright (c) Rod Kay 2007+
 --  AUSTRALIA
 --
 --  Permission granted to use this software, without any warranty,
@@ -9,49 +9,47 @@
 --  and unmodified if sources are distributed further.
 -------------------------------------------------------------------------
 
-with GLOBE_3D.tri_Mesh;
-with GL.Skinned_Geometry;
-with GL.Geometry.VBO;
---  with GL.Textures;
+with
+     GLOBE_3D.tri_Mesh,
+     GL.Skinned_Geometry,
+     GL.Geometry.VBO;
 
 package GLOBE_3D.tri_Mesh.VBO is
 
-   -- provides a triangle mesh Visual, using an openGL 'vertex buffer object' for geometry.
+   -- A triangle mesh Visual, using an openGL 'vertex buffer object' for geometry.
    --
-
    type tri_Mesh is new GLOBE_3D.tri_Mesh.tri_Mesh with
       record
-         skinned_Geometry : GL.Skinned_Geometry.Skinned_Geometry := (Geometry  => new GL.Geometry.VBO.vbo_Geometry'
-                                                                                        (GL.Geometry.Geometry with
-                                                                                         primitive_Id  => GL.TRIANGLES,
-                                                                                         vertex_Count  => 0,
-                                                                                         indices_Count => 0,
-                                                                                         others        => <>),
-                                                                     Skin      => null,
-                                                                     Veneer    => null);
+         skinned_Geometry : GL.Skinned_Geometry.Skinned_Geometry
+           := (Geometry  => new GL.Geometry.VBO.vbo_Geometry'
+                                  (GL.Geometry.Geometry with
+                                   primitive_Id  => GL.TRIANGLES,
+                                   vertex_Count  => 0,
+                                   indices_Count => 0,
+                                   others        => <>),
+               Skin      => null,
+               Veneer    => null);
       end record;
 
    type p_tri_Mesh       is access all tri_Mesh'Class;
    type p_tri_Mesh_array is array (Positive range <>) of p_tri_Mesh;
    type p_tri_Mesh_grid  is array (Positive range <>, Positive range <>) of p_tri_Mesh;
 
-   procedure destroy (o: in out tri_Mesh);
+   procedure destroy        (o : in out tri_Mesh);
+   procedure Pre_calculate  (o : in out tri_Mesh);
 
-   procedure Pre_calculate (o: in out tri_Mesh);
+   procedure Display        (o : in out tri_Mesh;   clip : in     Clipping_data);
 
-   procedure Display (o    : in out tri_Mesh;
-                      clip : in     Clipping_data);
-
-   procedure set_Alpha ( o    : in out tri_Mesh;   Alpha : in GL.Double);
-   function  is_Transparent (o    : in tri_Mesh) return Boolean;
+   procedure set_Alpha      (o : in out tri_Mesh;   Alpha : in GL.Double);
+   function  is_Transparent (o : in     tri_Mesh) return Boolean;
 
    procedure set_Vertices (Self : in out tri_Mesh;   To : access GL.Geometry.Vertex_array);
    procedure set_Indices  (Self : in out tri_Mesh;   To : access GL.Geometry.vertex_Id_array);
 
-   function skinned_Geometries (o : in tri_Mesh) return GL.Skinned_Geometry.Skinned_Geometries;
-   function face_Count         (o : in tri_Mesh) return Natural;
-   function Bounds             (o : in tri_Mesh) return GL.Geometry.Bounds_record;
+   function skinned_Geometries (o : in     tri_Mesh) return GL.Skinned_Geometry.Skinned_Geometries;
+   function face_Count         (o : in     tri_Mesh) return Natural;
+   function Bounds             (o : in     tri_Mesh) return GL.Geometry.Bounds_record;
 
-   procedure Skin_is (o : in out tri_Mesh;   Now : in GL.Skins.p_Skin);
+   procedure Skin_is           (o : in out tri_Mesh;   Now : in GL.Skins.p_Skin);
 
 end GLOBE_3D.tri_Mesh.VBO;
