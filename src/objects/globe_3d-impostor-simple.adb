337171167304be13e1d98ac5ff00c8ec522448d5
rodkay <rodkay@c4d228d6-d88a-4964-8f71-aa44024e0872>
2016-06-25 09:57:26 +0000

    Tidy of 'globe_3d.impostor.simple' spec & body

--- src/objects/globe_3d-impostor-simple.adb
+++ src/objects/globe_3d-impostor-simple.adb
@@ -1,14 +1,7 @@
---  with GLOBE_3D.Textures,
-with GLOBE_3D.Math;
-
---  with GLUT.Windows; use GLUT.Windows;
---  with GL.Errors;
---  with GLU;
+with
+      GLOBE_3D.Math;
 
 package body GLOBE_3D.Impostor.Simple is
-
-   --  package G3DT renames GLOBE_3D.Textures;
-   --  package G3DM renames GLOBE_3D.Math;
 
    procedure pre_Calculate (o : in out Impostor)
    is
@@ -32,7 +25,7 @@ package body GLOBE_3D.Impostor.Simple is
       use GL, GLOBE_3D.Math;
       world_Rotation_original : constant GLOBE_3D.Matrix_33 := the_Camera.world_rotation;
    begin
-      o.current_Camera_look_at_Rotation := Look_at (the_Camera.clipper.eye_position,     -- look directly at target so it will be rendered
+      o.current_Camera_look_at_Rotation := Look_at (the_Camera.clipper.eye_position,     -- Look directly at target so it will be rendered
                                                     o.Target.centre,                     -- in the centre of the viewport.
                                                     (the_Camera.world_rotation (2, 1), the_Camera.world_rotation (2, 2), the_Camera.world_rotation (2, 3)));
       the_Camera.world_rotation         := o.current_Camera_look_at_Rotation;
@@ -40,7 +33,6 @@ package body GLOBE_3D.Impostor.Simple is
 
       declare
          use GL.Textures;
-         --  use type GL.Double;
          update_Required : Boolean := o.general_Update_required (the_Camera, o.current_pixel_Region);
       begin
          if         not update_Required
@@ -50,7 +42,7 @@ package body GLOBE_3D.Impostor.Simple is
          end if;
 
          if update_Required then
-            o.current_Width_pixels  := o.current_pixel_Region.Width;       -- cache current state.
+            o.current_Width_pixels  := o.current_pixel_Region.Width;       -- Cache current state.
             o.current_Height_pixels := o.current_pixel_Region.Height;
 
             o.current_copy_X        := o.current_pixel_Region.X;
@@ -66,14 +58,14 @@ package body GLOBE_3D.Impostor.Simple is
       end;
    end;
 
-   procedure update (o : in out Impostor;   the_Camera   : in     p_Camera;
-                                            texture_Pool : in     GL.Textures.p_Pool)
+   procedure update (o : in out Impostor;   the_Camera   : in p_Camera;
+                                            texture_Pool : in GL.Textures.p_Pool)
    is
       use GL, GLOBE_3D.Math;
       world_Rotation_original : constant GLOBE_3D.Matrix_33 := the_Camera.world_rotation;
    begin
       the_Camera.world_rotation := o.current_Camera_look_at_Rotation;
-      GLOBE_3D.Impostor.Impostor (o).update (the_Camera, texture_Pool);   -- base class 'update'
+      GLOBE_3D.Impostor.Impostor (o).update (the_Camera, texture_Pool);   -- Call base class update().
       the_Camera.world_rotation := world_Rotation_original;
    end;
 
--- src/objects/globe_3d-impostor-simple.ads
+++ src/objects/globe_3d-impostor-simple.ads
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------------
 --  GLOBE_3D.Impostor.Simple
 --
---  Copyright (c) Rod Kay 2007
+--  Copyright (c) Rod Kay 2007+
 --  AUSTRALIA
 --
 --  Permission granted to use this software, without any warranty,
@@ -9,22 +9,22 @@
 --  and unmodified if sources are distributed further.
 -------------------------------------------------------------------------
 
-with GL, GL.Textures;
+with
+     GL.Textures;
 
 package GLOBE_3D.Impostor.Simple is      -- tbd: rename 'GLOBE_3D.Impostor.standard' ?
 
-   -- can impostor any 'visual'.
+   -- Can impostor any 'Visual'.
 
-   type Impostor   is new GLOBE_3D.Impostor.Impostor with private;
+   type   Impostor is new GLOBE_3D.Impostor.Impostor with private;
    type p_Impostor is access all Impostor'Class;
 
-   procedure pre_Calculate (o : in out Impostor);
+   procedure pre_Calculate   (o : in out Impostor);
 
    function  update_Required (o : access Impostor;   the_Camera   : in     GLOBE_3D.p_Camera) return Boolean;
    procedure update          (o : in out Impostor;   the_Camera   : in     p_Camera;
                                                      texture_Pool : in     GL.Textures.p_Pool);
-
-   procedure free    (o : in out p_Impostor);
+   procedure free            (o : in out p_Impostor);
 
 private
 
