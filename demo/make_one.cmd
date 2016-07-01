ef5d713109fc309a453fc53183a7c70ac116d63b
gdemont <gdemont@c4d228d6-d88a-4964-8f71-aa44024e0872>
2016-07-01 00:16:06 +0000

    updated make_one.cmd for the case it is invoked prior to any use of GPS: GPS creates the 'build' directory, gprbuld doesn't itself.

--- demo/make_one.cmd
+++ demo/make_one.cmd
@@ -16,6 +16,7 @@ echo -----------------------------------------------
 echo ---- Compile mode: %scenario_324943981475%
 echo -----------------------------------------------
 
+md build
 gprbuild -PGLOBE_3D_Demos_Win32.gpr %1.adb -d -XOS=win32 -XBuild_Mode=%scenario_324943981475%
 
 set scenario_324943981475=
