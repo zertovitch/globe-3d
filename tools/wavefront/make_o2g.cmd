c65a11ed2e32cd491c20ad53640f62a55b0a8b6b
gdemont <gdemont@c4d228d6-d88a-4964-8f71-aa44024e0872>
2016-07-04 16:40:16 +0000

    Tools: OS-agnostic GNAT project file for the Wavefront converter

--- tools/wavefront/make_o2g.cmd
+++ tools/wavefront/make_o2g.cmd
@@ -1,1 +1,12 @@
-gprbuild -Po2g.gpr
+@echo off
+echo.
+echo make_o2g: option -O builds with speed-oriented optimizations
+echo.
+
+              set opt_comp294572=debug
+if "%1"=="-O" set opt_comp294572=fast
+
+gprbuild -Po2g.gpr -XBuild_Mode=%opt_comp294572%
+
+set opt_comp294572=
+
