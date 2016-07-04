b08cca413dabc6bc86ddba517a81a0415e2c3027
gdemont <gdemont@c4d228d6-d88a-4964-8f71-aa44024e0872>
2016-07-04 15:38:46 +0000

    Object directory for globe_3d_demos_win32.gpr same as rest of the build

--- demo/make_one.cmd
+++ demo/make_one.cmd
@@ -16,7 +16,6 @@ echo -----------------------------------------------
 echo ---- Compile mode: %scenario_324943981475%
 echo -----------------------------------------------
 
-md gnat_build_files
 gprbuild -PGLOBE_3D_Demos_Win32.gpr %1.adb -d -XOS=win32 -XBuild_Mode=%scenario_324943981475%
 
 set scenario_324943981475=
