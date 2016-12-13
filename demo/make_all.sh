09e83f7a2cf49d183576fd7bfd97fbe335fc9c97
gdemont <gdemont@c4d228d6-d88a-4964-8f71-aa44024e0872>
2016-12-13 15:45:27 +0000

    Fixed disappeared '/' from make_all.sh

--- demo/make_all.sh
+++ demo/make_all.sh
@@ -1,18 +1,18 @@
-#!binsh
-# Author John Howard Echosoft.LLC@gmail.com
-# Version 20161004
-# Execute this Unix shell script (.make_all.sh) from the demo directory.
+#!/bin/sh
+# Author: John Howard <Echosoft.LLC@gmail.com>
+# Version: 20161004
+# Execute this Unix shell script (./make_all.sh) from the demo directory.
 gprbuild -P globe_3d_demos.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
-gprbuild -P cullerarmadaarmada.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
-gprbuild -P cullerterrain_vboterrain_vbo.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
-gprbuild -P multi_windowmulti_window.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
-gprbuild -P spritesprite_demo.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
-gprbuild -P ..toolsglobe_3d_tools.gpr -XOS_Kind=linux -XBuild_Mode=fast
-gprbuild -P ..toolsdoom3to_adad3a.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
-gprbuild -P ..toolsdoom3to_g3dd3g.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
-gprbuild -P ..toolsvrmlw2a.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
-gprbuild -P ..toolswavefronto2g.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
-cd ..test
+gprbuild -P culler/armada/armada.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
+gprbuild -P culler/terrain_vbo/terrain_vbo.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
+gprbuild -P multi_window/multi_window.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
+gprbuild -P sprite/sprite_demo.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
+gprbuild -P ../tools/globe_3d_tools.gpr -XOS_Kind=linux -XBuild_Mode=fast
+gprbuild -P ../tools/doom3/to_ada/d3a.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
+gprbuild -P ../tools/doom3/to_g3d/d3g.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
+gprbuild -P ../tools/vrml/w2a.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
+gprbuild -P ../tools/wavefront/o2g.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
+cd ../test/
 gnatmake dico_drill.adb
-gprbuild -P vertex_buffer_objectgeneralsimple.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
-cd ..demo
+gprbuild -P vertex_buffer_object/general/simple.gpr -d -XOS_Kind=linux -XBuild_Mode=fast
+cd ../demo/
