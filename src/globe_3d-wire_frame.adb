7907fa3780013961e79ebbf8f6c84b6b8fcbd1f7
unknown <gdemont@hotmail.com>
2023-10-22 20:51:19 +0200

    Wire frame object: code gardening + added `Destroy` method

--- src/globe_3d-wire_frame.adb
+++ src/globe_3d-wire_frame.adb
@@ -1,25 +1,35 @@
 with GLOBE_3D.Math;
 
-package body GLOBE_3D.Wire_frame is
+package body GLOBE_3D.Wire_Frame is
 
-  overriding procedure Display_one(o: in out Wired_3D) is
+  overriding procedure Destroy (o : in out Wired_3D) is
+    procedure Dispose is
+      new Ada.Unchecked_Deallocation (Segment_Array, p_Segment_Array);
   begin
-    -- Call parent method:
-    Display_one(Object_3D(o));
-    -- Display the wires:
+    --  Destroy the wires:
+    Dispose (o.wire);
+    --  Call parent method:
+    Destroy (Object_3D (o));
+  end Destroy;
+
+  overriding procedure Display_One (o : in out Wired_3D) is
+  begin
+    --  Call parent method:
+    Display_One (Object_3D (o));
+    --  Display the wires:
     if o.wire /= null then
       GL.PushMatrix;
-      GL.Translate( o.centre );
-      Math.Multiply_GL_Matrix(o.rotation);
+      GL.Translate (o.centre);
+      Math.Multiply_GL_Matrix (o.rotation);
       for i in o.wire'Range loop
-        GL.Color(o.wire(i).colour);
-        GL.GL_Begin(GL.LINES);
-        GL.Vertex(o.point(o.wire(i).P1));
-        GL.Vertex(o.point(o.wire(i).P2));
+        GL.Color (o.wire (i).colour);
+        GL.GL_Begin (GL.LINES);
+        GL.Vertex (o.point (o.wire (i).P1));
+        GL.Vertex (o.point (o.wire (i).P2));
         GL.GL_End;
       end loop;
       GL.PopMatrix;
     end if;
-  end Display_one;
+  end Display_One;
 
-end GLOBE_3D.Wire_frame;
+end GLOBE_3D.Wire_Frame;
--- src/globe_3d-wire_frame.ads
+++ src/globe_3d-wire_frame.ads
@@ -1,22 +1,26 @@
-package GLOBE_3D.Wire_frame is
+--  Wired_3D adds a wire-frame shape to a standard 3D object.
+
+package GLOBE_3D.Wire_Frame is
 
   type Segment is record
-    P1, P2: Positive; -- Indices to o.Point
-    colour: GL.RGBA_Color;
+    P1, P2 : Positive;  --  Indices to o.Point
+    colour : GL.RGBA_Color;
   end record;
 
-  type Segment_array is array(Positive range <>) of Segment;
-  type p_Segment_array is access Segment_array;
+  type Segment_Array is array (Positive range <>) of Segment;
+  type p_Segment_Array is access Segment_Array;
 
   type Wired_3D is new Object_3D with record
-    wire: p_Segment_array:= null;
+    wire : p_Segment_Array := null;
   end record;
 
   type p_Wired_3D is access Wired_3D;
 
-  type Wired_3D_array is array(Positive range <>) of p_Wired_3D;
-  type p_Wired_3D_array is access Wired_3D_array;
+  type Wired_3D_Array is array (Positive range <>) of p_Wired_3D;
+  type p_Wired_3D_Array is access Wired_3D_Array;
 
-  overriding procedure Display_one(o: in out Wired_3D);
+  overriding procedure Destroy (o : in out Wired_3D);
 
-end GLOBE_3D.Wire_frame;
+  overriding procedure Display_One (o : in out Wired_3D);
+
+end GLOBE_3D.Wire_Frame;
